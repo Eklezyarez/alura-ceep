2155e0156304beb47ff72c166c9331e889ee717c
Eklezyarez <eklezyarez@gmail.com>
2022-08-18 19:45:09 -0300

    IIFE

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,4 +1,7 @@
-const criarTarefa = (evento)=>{
+(() => {
+    
+    const criarTarefa = (evento) => {
+
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
@@ -7,15 +10,40 @@ const criarTarefa = (evento)=>{
 
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild(tarefa)
     input.value = " "
+
 }
+
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => { 
+    const botaoConclui = document.createElement('button')  
+    
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+
+}
+
+
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
+})()
