ea8ad2c0ae2cad4f915b1535baf5cf0640f0261b
Eklezyarez <eklezyarez@gmail.com>
2022-08-18 18:55:45 -0300

    file js

---
+++ listaDeTarefas.js
@@ -0,0 +1,10 @@
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', ()=> {
+
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    console.log(valor)
+    input.value = " "
+})
