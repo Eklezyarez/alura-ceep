ae2430823a7ab2949a0e3486d900639121b24e62
Eklezyarez <eklezyarez@gmail.com>
2022-09-03 18:16:01 -0300

    localStorage

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const handleNovoItem = (evento) => {
 
     lista.appendChild(criaTarefa)
 
-    sessionStorage.setItem('tarefas', JSON.stringify(dados))
+    localStorage.setItem('tarefas', JSON.stringify(dados))
     input.value = " "
 }
 
