ac0a6c8dfa732b8a6c6a57847f6f7024c46f1418
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-09-04 21:17:43 -0300

    Update criaTarefa.js

--- criaTarefa.js
+++ criaTarefa.js
@@ -3,6 +3,11 @@ import BotaoDeleta from './deletaTarefa.js'
  
 
 export const handleNovoItem = (evento) => {
+  evento.preventDefault()
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
    
 
     sessionStorage
